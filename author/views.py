90f53d0be96815ea31030e7918ea71fa03224d73
Oleksandra <aleksandra@regdesk.co>
2024-04-24 16:50:45 +0300

    Solution

--- author/views.py
+++ author/views.py
@@ -1,6 +1,8 @@
-from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
+from author.models import Author
+from author.serializers import AuthorSerializer
 
 
-class AuthorViewSet(viewsets.ModelViewSet):
-    # write your code here
-    pass
+class AuthorViewSet(ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
